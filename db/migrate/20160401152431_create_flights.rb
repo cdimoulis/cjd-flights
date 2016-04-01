cde803101a07604c7bb2eb4c6cf3d3961b5d2dd0
Chris Dimoulis <christopherjamesd@gmail.com>
2016-04-01 10:40:10 -0500

    [ADDED] enabled UUID and added flights model

---
+++ db/migrate/20160401152431_create_flights.rb
@@ -0,0 +1,20 @@
+class CreateFlights < ActiveRecord::Migration
+  def change
+    create_table :flights, id: :uuid, default: "uuid_generate_v4()", force: true do |t|
+      t.uuid :airline_id, null: false
+      t.string :number, null: false
+      t.date :departure_date, null: false
+      t.date :arrival_date, null: false
+      t.time :departure_time, null: false
+      t.time :arrival_time, null: false
+      t.uuid :departure_airpot, null: false
+      t.uuid :arrival_airport, null: false
+      t.string :cabin_code, null: false
+      t.string :fare_basis
+      t.string :aircraft
+      t.text :notes
+
+      t.timestamps null: false
+    end
+  end
+end
