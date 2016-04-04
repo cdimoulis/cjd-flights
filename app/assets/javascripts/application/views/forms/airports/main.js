e77db3b1212f3a2b01a5a8ca62c26a562d9fb30e
Chris Dimoulis <christopherjamesd@gmail.com>
2016-04-03 20:02:04 -0500

    [ADDED] timezone selector

--- app/assets/javascripts/application/views/forms/airports/main.js
+++ app/assets/javascripts/application/views/forms/airports/main.js
@@ -9,6 +9,7 @@ App.View.extend({
   ],
   init_functions: [
     'setup',
+    'setupTimezones',
     'setupComponents',
   ],
 
@@ -28,6 +29,50 @@ App.View.extend({
         _this.$el.find('span.new').addClass('active', 400);
       }
     });
+  },
+
+  setupTimezones: function() {
+    this.selected_timezone = new App.Model();
+    this.timezones = new App.Collection([
+      {zone: "-12:00"},
+      {zone: "12:00"},
+      {zone: "00:00"},
+      {zone: "00:30"},
+      {zone: "-00:30"},
+    ]);
+    this.timezones.comparator = function(a, b) {
+      x = parseInt(a.get('zone'));
+      y = parseInt(b.get('zone'));
+
+      if (x == y) {
+        if (b.get('zone').split(':')[1] == '30') {
+          return x < 0 ? 1 : -1
+        }
+
+        if (a.get('zone').split(':')[1] == '30') {
+          return x < 0 ? -1 : 1
+        }
+      }
+      else {
+        return x < y ? -1 : 1
+      }
+    }
+
+    var hour = 11;
+
+    while (hour != 0) {
+      z1 = moment(hour+':30','h:m').format('hh:mm');
+      z2 = moment(hour+':00','h:m').format('hh:mm');
+
+      if (hour != 0) {
+        this.timezones.add({zone: z1});
+        this.timezones.add({zone: '-'+z1});
+        this.timezones.add({zone: z2});
+        this.timezones.add({zone: '-'+z2});
+      }
+      hour--;
+    }
+    window.timezones = this.timezones;
   },
 
   setupComponents: function() {
@@ -61,6 +106,13 @@ App.View.extend({
       model: this.airport,
       attribute: 'country',
       label: "Country",
+    };
+
+    c.timezone = {
+      collection: this.timezones,
+      attribute: 'zone',
+      selected: this.selected_timezone,
+      label: "Timezone",
     };
 
     c.clear = {
